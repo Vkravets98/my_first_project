84ae03c43b61b996abb5c9bb490ea18f818545a2
Kostya <mkv150293@gmail.com>
2020-12-16 17:13:50 +0300

    zima is a function that helps us always dress for the weather

--- Vkravets98/first.py
+++ Vkravets98/first.py
@@ -2,7 +2,9 @@ def zima():
     t = int(input('The temperature is: '))
     if t > 0:
         return 'Take off your hat'
-    else:
+    elif t == 0:
         return'Put on your hat'
+    else:
+        return 'Stay home'
 print(zima())
 
